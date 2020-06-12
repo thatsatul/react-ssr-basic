342c3764f9773948c73c170327630f06e53d6f1d
Atul Anand <atul.anand@travenues.com>
2020-06-12 15:38:00 +0530

    Setting environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: '[name].js',
-    sourceMapFilename: "[name].js.map"
-  },
-  devtool: "source-map",
-  devServer: {
-    contentBase: path.join(__dirname),
-    compress: true,
-    port: 8080,
-    historyApiFallback: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-        ]
-      }
+module.exports = env => {
+  console.log('Running application in environment: ', env.NODE_ENV);
+
+  return {
+    mode: env.NODE_ENV, // development || production
+    entry: './src/index.js',
+    output: {
+      filename: env.NODE_ENV === 'development' ? '[name].js' : 'bundle.js',
+      sourceMapFilename: '[name].js.map'
+    },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname),
+      compress: true,
+      port: 8080,
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'template.html'
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html'
-    })
-  ]
+  };
 };
