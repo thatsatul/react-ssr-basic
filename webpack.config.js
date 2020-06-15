5518bac073d829d195ecb999d0acbbbb894b38be
Atul Anand <atul.anand@travenues.com>
2020-06-15 15:02:25 +0530

    port issue fix trial

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = env => {
     devServer: {
       contentBase: path.join(__dirname),
       compress: true,
-      port: process.env.PORT || env.PORT || 8080,
-      host: '0.0.0.0',
+      port: process.env.PORT,
+      host: process.env.HOST || env.HOST || '0.0.0.0',
       historyApiFallback: true,
       disableHostCheck: true
     },
